d8b315fab1f8256bf10b2df4c491a0d76d5efdea
Paleolithic <tjb2597@rit.edu>
2014-05-13 00:30:54 -0400

    First commit

---
+++ entry-meta.php
@@ -0,0 +1,7 @@
+<section class="entry-meta">
+	<span class="author vcard">Posted by <?php the_author_posts_link(); ?> in <?php the_category( ', ' ); ?></span>
+	<span class="tag-links"><i class="fa fa-tags"></i> <?php the_tags(); ?></span>
+	<span class="comments-link"><i class="fa fa-comments"></i> <a href="' . get_comments_link() . '"><?php comments_number( "0", "1", "%" ); ?></a></span>
+	<!-- <span class="meta-sep"> | </span> -->
+	<!-- <span class="entry-date"><?php the_time( get_option( 'date_format' ) ); ?></span> -->
+</section>
---
+++ entry.php
@@ -0,0 +1,16 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div class="two columns alpha">
+	<div class="entry-date">
+		<div id='date'><?php the_time('M'); ?><br> <span id='day'><?php the_time('d'); ?></span></div>
+	</div>
+</div>
+<div class="ten columns omega">
+	<header>
+		<?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?> <?php edit_post_link(); ?>
+		<?php if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
+		
+	</header>
+	<?php get_template_part( 'entry', ( is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
+	<?php if ( !is_search() ) get_template_part( 'entry-footer' ); ?>
+</div>
+</article>
