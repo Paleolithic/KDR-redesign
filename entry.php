a59988becb1716deee47c6989011cbca3c1b0295
Paleolithic <tjb2597@rit.edu>
2014-07-09 00:26:39 -0400

    Single post page formatting

--- entry.php
+++ entry.php
@@ -1,10 +1,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="one column alpha">
+	<div class="two columns alpha">
 		<div class="entry-date">
 			<div id='date'><?php the_time('M'); ?><br> <span id='day'><?php the_time('d'); ?></span></div>
 		</div>
 	</div>
-	<div class="eleven columns omega">
+	<div class="ten columns omega">
 		<header>
 			<?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?> <?php edit_post_link(); ?>
 			<?php if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
