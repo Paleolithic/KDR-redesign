162c8296efbe8ea8629b3e93f6f986e89a693510
Paleolithic <tjb2597@rit.edu>
2014-07-29 01:17:11 -0400

    Map and e-board begun

--- template-about.php
+++ template-about.php
@@ -8,7 +8,7 @@ Template Name: About us page
 	<div class="top">
 		<div class="container">
 			<div class="six columns offset-by-five">
-				<h5><span>Welcome to</span></h5>
+				<h5>Welcome to</h5>
 				<h1>K&Delta;P</h1>
 				<h5>Iota Beta Chapter</h5>
 			</div>
@@ -16,34 +16,222 @@ Template Name: About us page
 	</div>
 	<div class="info">
 		<div class="container">
-			<div class="fourteen columns offset-by-one">
+			<div class="fourteen columns offset-by-one info-title">
 				<h1>About our Fraternity</h1>
 				<h5>Nam adipiscing. Nullam dictum felis eu pede mollis pretium. Donec mi odio, faucibus at, scelerisque quis, convallis in, nisi. Nullam vel sem. Aliquam eu nunc.
 				</h5>
 			</div>
 			<div class="one-third column info-thirds">
-				<?php get_template_part('Eldorado/inline', 'radio.svg'); ?>
+				<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="64px" height="64px" viewBox="0 0 40 40" style="enable-background:new 0 0 40 40;" xml:space="preserve">
+					<g>
+						<g>
+							<path d="M36.953,19.245c0.044-0.115,0.084-0.231,0.121-0.348c0.387-1.274,0.244-2.592-0.4-3.711
+								c-0.546-0.948-1.396-1.654-2.467-2.036c0.043-2.163-1.418-4.081-3.611-4.598c-1.328-0.258-2.543,0.078-3.299,0.386
+								c-0.924-2.828-3.576-4.606-6.234-4.204c-1.267-2.422-3.904-3.736-6.34-3.081c-2.434,0.652-4.06,3.102-3.949,5.83
+								c-2.619,0.996-4.061,4.078-3.244,7.129c0.052,0.192,0.112,0.381,0.18,0.566c-0.234,0.295-0.438,0.614-0.609,0.952
+								c-1.598-0.024-2.485,0.571-3.079,1.116c-1.342,1.225-1.598,3.306-0.68,5.024c-1.259,1.547-1.125,3.955,0.396,5.618
+								c0.068,0.074,0.14,0.147,0.214,0.218c-0.23,1.333,0.207,2.773,1.191,3.85c0.764,0.834,1.781,1.37,2.844,1.507
+								c0.208,0.461,0.49,0.888,0.842,1.273c0.793,0.867,1.844,1.407,2.96,1.521c1.008,0.104,1.961-0.149,2.717-0.708
+								c1.352,0.764,2.942,0.888,4.346,0.321l-0.5-1.236c-1.123,0.454-2.472,0.28-3.522-0.456l-0.519-0.269l-0.396,0.4
+								c-0.521,0.478-1.23,0.697-1.99,0.62c-0.79-0.081-1.541-0.47-2.112-1.095c-0.332-0.362-0.576-0.773-0.729-1.221l-0.15-0.441
+								l-0.465-0.01c-0.861-0.02-1.713-0.423-2.341-1.108c-0.778-0.852-1.084-2.002-0.797-3.003L5.45,27.63l-0.342-0.278
+								c-0.143-0.116-0.27-0.236-0.388-0.365c-1.16-1.269-1.208-3.121-0.097-4.138l0.431-0.393l-0.332-0.479
+								c-0.863-1.246-0.778-2.857,0.199-3.749c0.385-0.354,0.86-0.677,1.687-0.752c-0.243,1.024-0.233,2.131,0.056,3.207
+								c0.415,1.548,1.363,2.877,2.631,3.711c-0.059,0.747,0.011,1.498,0.209,2.239c0.834,3.108,3.716,5.069,6.527,4.524
+								c1.16,1.81,3.258,3.01,4.771,3.246c0.728,0.113,1.326,0.102,1.828,0.031c0.074,1.023,0.128,2.064,0.161,3.125l1.333-0.042
+								c-0.037-1.163-0.098-2.303-0.182-3.422c0.045-0.016,0.088-0.032,0.131-0.047c0.098-0.035,0.193-0.069,0.285-0.1
+								c1.791-0.595,3.842-3.359,3.984-6.089c2.711-0.936,4.223-4.075,3.391-7.183c-0.199-0.742-0.514-1.428-0.939-2.043
+								c0.684-1.354,0.839-2.979,0.423-4.529c-0.477-1.782-1.655-3.252-3.175-4.027c0.58-0.207,1.403-0.39,2.272-0.221
+								c1.711,0.404,2.772,1.953,2.522,3.685l-0.04,0.614l0.594,0.14c0.917,0.216,1.672,0.769,2.125,1.557
+								c0.458,0.796,0.558,1.741,0.28,2.658c-0.06,0.195-0.138,0.39-0.23,0.577l-0.22,0.442l0.359,0.339
+								c0.906,0.856,1.236,2.155,0.859,3.391c-0.31,1.02-1.057,1.879-2.048,2.359l-0.452,0.218l0.084,0.494
+								c0.089,0.523,0.053,1.056-0.107,1.583c-0.584,1.922-2.641,3.111-4.581,2.653c-0.025-0.006-0.157-0.028-0.299-0.049
+								c-0.111-0.017-0.229-0.035-0.24-0.035l-0.349,1.286c0.031,0.009,0.216,0.041,0.388,0.067c0.093,0.014,0.181,0.027,0.193,0.029
+								c2.609,0.618,5.378-0.98,6.164-3.563c0.172-0.567,0.239-1.141,0.202-1.711c1.115-0.668,1.948-1.719,2.32-2.942
+								C38.319,22.082,37.977,20.437,36.953,19.245z M29.931,14.449c0.364,1.355,0.183,2.773-0.496,3.89l-0.242,0.397l0.29,0.364
+								c0.447,0.562,0.771,1.208,0.963,1.921c0.676,2.523-0.574,5.059-2.773,5.649l-0.684,0.156l0.024,0.557
+								c0.096,2.231-1.639,4.822-3.075,5.3c-0.037,0.012-0.074,0.025-0.112,0.038c-0.221-2.375-0.539-4.638-0.922-6.774
+								c1.965-0.631,3.467-2.078,4.128-3.979l-1.259-0.438c-0.509,1.464-1.656,2.588-3.117,3.096c-0.391-1.988-0.834-3.857-1.302-5.588
+								c1.741-0.7,3.011-2.098,3.501-3.868l-1.285-0.356c-0.369,1.329-1.313,2.388-2.576,2.934c-0.522-1.816-1.063-3.463-1.586-4.92
+								c1.025-0.539,1.74-1.469,1.977-2.585l-1.305-0.277c-0.145,0.679-0.565,1.254-1.137,1.601c-1.481-3.908-2.707-6.149-2.736-6.203
+								l-1.166,0.647c0.029,0.052,1.203,2.2,2.631,5.954c-0.656,0.041-1.297-0.161-1.797-0.592l-0.87,1.011
+								c0.847,0.728,1.981,1.039,3.141,0.861c0.517,1.438,1.054,3.069,1.571,4.869c-1.463,0.22-2.875-0.218-3.905-1.218l-0.929,0.956
+								c1.342,1.304,3.257,1.879,5.192,1.551c0.46,1.708,0.897,3.551,1.282,5.516c-1.682,0.175-3.264-0.41-4.361-1.62l-0.987,0.896
+								c1.379,1.521,3.44,2.278,5.594,2.035c0.386,2.163,0.705,4.461,0.92,6.874c-0.413,0.066-0.899,0.08-1.515-0.017
+								c-1.305-0.203-3.229-1.409-4.061-3.004l-0.287-0.477l-0.631,0.186c-2.212,0.593-4.56-0.98-5.237-3.502
+								c-0.19-0.712-0.233-1.434-0.128-2.146l0.068-0.458l-0.406-0.224c-1.146-0.63-2.012-1.769-2.375-3.122
+								c-0.448-1.673-0.059-3.427,1.015-4.578l0.299-0.321l-0.177-0.402c-0.108-0.246-0.199-0.506-0.271-0.771
+								c-0.677-2.523,0.572-5.058,2.822-5.66l0.578-0.141l-0.075-0.591c-0.291-2.288,0.966-4.408,2.925-4.933
+								c1.959-0.526,4.108,0.683,5,2.809l0.246,0.531l0.563-0.15c2.208-0.592,4.559,0.979,5.236,3.503
+								c0.068,0.252,0.118,0.518,0.149,0.787l0.051,0.437l0.421,0.125C28.255,11.435,29.48,12.763,29.931,14.449z"/>
+						</g>
+					</g>
+				</svg>
 				<h3>Honor</h3>
-				<h5>Pellentesque commodo eros a enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</h5>
+				<h5>Pellentesque commodo eros a enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae</h5>
 
 			</div>
 			<div class="one-third column info-thirds">
-				<?php get_template_part('Eldorado/inline', 'bell.svg'); ?>
+				<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+				 width="64px" height="64px" viewBox="0 0 40 40" style="enable-background:new 0 0 40 40;" xml:space="preserve">
+					<g>
+						<g>
+							<path d="M29.738,28.404V22h-1.333v6.404H22v1.333h6.405v6.405h1.333v-6.405h6.405v-1.333H29.738z M2.551,27.714v-1.989
+								c0-0.65,0.328-1.199,0.901-1.506l10.155-5.478c0.742,0.256,1.528,0.395,2.344,0.395c0.831,0,1.631-0.144,2.384-0.409l7.345,3.994
+								l0.637-1.171l-6.505-3.537c2.446-1.523,4.106-4.443,4.106-7.792c0-4.915-3.574-8.914-7.967-8.914
+								c-4.394,0-7.968,3.999-7.968,8.914c0,3.361,1.672,6.291,4.133,7.81l-9.295,5.013c-1.004,0.539-1.604,1.541-1.604,2.681v1.989
+								c0,1.679,1.366,3.044,3.046,3.044H19v-1.333H4.263C3.319,29.425,2.551,28.657,2.551,27.714z M9.316,10.221
+								c0-4.18,2.976-7.58,6.635-7.58c3.658,0,6.634,3.4,6.634,7.58c0,4.181-2.976,7.582-6.634,7.582
+								C12.292,17.803,9.316,14.402,9.316,10.221z"/>
+						</g>
+					</g>
+				</svg>
 				<h3>Super</h3>
-				<h5>Pellentesque commodo eros a enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</h5>
+				<h5>Pellentesque commodo eros a enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae</h5>
 			</div>
 			<div class="one-third column info-thirds">
-				<?php get_template_part('Eldorado/inline', 'radio.svg'); ?>
+				<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+				 width="64px" height="64px" viewBox="0 0 40 40" style="enable-background:new 0 0 40 40;" xml:space="preserve">
+					<g>
+						<g>
+							<path d="M8.706,10.493l-1.721,2.298v0.543H2c-0.368,0-0.667,0.298-0.667,0.667S1.632,14.667,2,14.667h4.985v0.568l1.722,2.298
+								c0.556,0.739,1.369,1.146,2.293,1.146h5v-1.333h-5c-0.501,0-0.925-0.212-1.226-0.613l-1.455-1.942v-0.548
+								C8.349,14.167,8.368,14.086,8.368,14s-0.02-0.167-0.049-0.242v-0.523l1.454-1.942c0.306-0.407,0.718-0.614,1.227-0.614h5V9.345h-5
+								C10.076,9.345,9.262,9.752,8.706,10.493z M25.333,17.013h1.334v-6.001h-1.334V17.013z M21.333,17.013h1.334v-6.001h-1.334V17.013z
+								 M29.325,17.013h1.334v-6.001h-1.334V17.013z M36.465,6.345H19.547c-1.216,0-2.206,0.99-2.206,2.207v10.922
+								c0,1.217,0.99,2.207,2.206,2.207h6.188l0.416,1.667h-0.044c-0.976,0-1.769,0.792-1.769,1.767v0.8c0,0.975,0.793,1.767,1.769,1.767
+								h1.128l0.199,0.799c-0.643,0.265-1.096,0.898-1.096,1.635v0.8c0,0.974,0.793,1.766,1.767,1.766h5.926
+								c0.694,0,1.312-0.301,1.741-0.849c0.426-0.548,0.568-1.22,0.399-1.892l-2.064-8.26h2.357c1.216,0,2.206-0.99,2.206-2.207V8.552
+								C38.671,7.335,37.681,6.345,36.465,6.345z M37.337,19.474c0,0.481-0.391,0.874-0.872,0.874H32.4l2.479,9.917
+								c0.069,0.278,0.018,0.523-0.157,0.749c-0.178,0.226-0.403,0.336-0.69,0.336h-5.926c-0.239,0-0.433-0.194-0.433-0.433v-0.8
+								c0-0.239,0.193-0.434,0.433-0.434h1.005l-0.834-3.333h-2.169c-0.239,0-0.435-0.195-0.435-0.434v-0.8
+								c0-0.239,0.195-0.434,0.435-0.434h1.753l-1.084-4.333h-7.229c-0.481,0-0.872-0.392-0.872-0.874V8.552
+								c0-0.481,0.391-0.874,0.872-0.874h16.918c0.481,0,0.872,0.392,0.872,0.874V19.474z M33.333,17.013h1.334v-6.001h-1.334V17.013z"/>
+						</g>
+					</g>
+				</svg>
 				<h3>Omnia</h3>
-				<h5>Pellentesque commodo eros a enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</h5>
-
+				<h5>Pellentesque commodo eros a enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae</h5>
 			</div>
 		</div>
 	</div>
-</div>
-<!-- <div class="container" id="container">
-	<section class="sixteen columns" id="content" role="main">
+	<div class="where">
+		<div id="map"></div>
+	</div>
+	<div class="blogs">
+		<div class="container">
+			<h1 class="sixteen columns">This is where Blogs or possibly pictures will go. Try left/right alternating</h1>
+		</div>
+	</div>
+	<div class="e-board">
+		<div class="container">
+			<div class="fourteen columns offset-by-one e-board-title">
+				<h1>Meet Our E-Board</h1>
+				<h5>Nam adipiscing. Nullam dictum felis eu pede mollis pretium. Donec mi odio, faucibus at, scelerisque quis, convallis in, nisi. Nullam vel sem. Aliquam eu nunc.
+				</h5>
+			</div>
+			<div class="one-fifth column e-board-fifths">
+				<img src="http://placekitten.com/172/206"/>
+				<h5>Lorem Dicksum</h5>
+				<h6>Consul</h6>
+			</div>
+			<div class="one-fifth column e-board-fifths">
+				<img src="http://placekitten.com/172/206"/>
+				<h5>Lorem Dicksum</h5>
+				<h6>Consul</h6>
+			</div>
+			<div class="one-fifth column e-board-fifths">
+				<img src="http://placekitten.com/172/206"/>
+				<h5>Lorem Dicksum</h5>
+				<h6>Consul</h6>
+			</div>
+			<div class="one-fifth column e-board-fifths">
+				<img src="http://placekitten.com/172/206"/>
+				<h5>Lorem Dicksum</h5>
+				<h6>Consul</h6>
+			</div>
+			<div class="one-fifth column e-board-fifths">
+				<img src="http://placekitten.com/172/206"/>
+				<h5>Lorem Dicksum</h5>
+				<h6>Consul</h6>
+			</div>
+		</div>
+	</div>
 
-	</section>
-</div> -->
+</div>
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDSNWPbzxo6_7n7uHyLs2f8Nx8ZYcmsP4M&sensor=false"></script>
+<script type="text/javascript">            
+	// When the window has finished loading create our google map below
+    google.maps.event.addDomListener(window, 'load', init);
+
+    function init() {
+        // Basic options for a simple Google Map
+        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+        var mapOptions = {
+            // How zoomed in you want the map to start at (always required)
+            zoom: 13,
+            scrollwheel: false,
+            streetViewControl: false,
+            panControl: false,
+            mapTypeControl: false,
+            center: new google.maps.LatLng(40.814, -73.9597),
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.SMALL
+            },
+            
+        };
+
+
+        // Get the HTML DOM element that will contain your map 
+        // We are using a div with id="map" seen below in the <body>
+        var mapElement = document.getElementById('map');
+
+        // Create the Google Map using out element and options defined above
+        var map = new google.maps.Map(mapElement, mapOptions);
+        var getCen = map.getCenter();
+		google.maps.event.addDomListener(window, 'resize', function() {
+			map.setCenter(getCen);
+		});
+
+		var contentString = 
+			'<div id="iwindow-descriptor" class="descriptor">'+
+				'<img src="">'+
+				'<h1 id="firstHeading" class="firstHeading">Where We\'re Located</h1>'+
+				'<div id="bodyContent">'+
+					'<h6>Nam eget dui. Praesent blandit laoreet nibh. Etiam ultricies nisi vel augue. Fusce risus nisl, viverra et, tempor et, pretium in, sapien. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus.</h6>'+
+				'</div>'+
+				'<div class="bottom"></div>'+
+			'</div>';
+
+		var infowindow = new google.maps.InfoWindow({
+		  content: contentString,
+		  maxWidth: 460
+		});
+
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(40.8, -73.9597),
+            map: map,
+            animation: google.maps.Animation.DROP,
+            title: 'Field House'
+        });
+
+        infowindow.open(map,marker);
+
+
+	    google.maps.event.addListener(infowindow, 'domready', function() {
+	    	$l = $('#iwindow-descriptor').parent().parent().parent();
+	        $l.addClass('iwindow');
+	     });
+        // var mapWidth = document.getElementById('map').style.width;
+        // document.getElementById('map').style.height = mapWidth * .5;
+
+        // $("#map").height($("#height").height()-20);
+    }
+
+</script>
+
 <?php get_footer(); ?>
