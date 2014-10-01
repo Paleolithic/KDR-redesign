caff02fe10714caff5744777d3daeb76d3ef5795
Paleolithic <tjb2597@rit.edu>
2014-10-01 01:48:25 -0400

    Content added

--- template-history.php
+++ template-history.php
@@ -10,7 +10,7 @@ Template Name: History page
 		<div class="container">
 			<header class="fourteen columns offset-by-one calendar-title">
 				<h1 class='lobster'>Chapter History</h1>
-				<p>Below you'll find our rush schedule, come hang out with us! All events start KDR House, located in Res Hall A</p>
+				<p>Read all about our vibrant history to find out more about how we became who we are today.</p>
 			</header>
 		</div>
 	</section>
@@ -21,23 +21,137 @@ Template Name: History page
 				<li class="cd-service cd-service-divider"></li>
 
 				<li class="cd-service cd-service-1">
-					<h2>Web Design</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis pariatur tenetur quod veritatis nulla aspernatur architecto! Fugit, reprehenderit amet deserunt molestiae ut libero facere quasi velit perferendis ullam quis necessitatibus!</p>
+					<h2>The Early Years</h2>
+					<p>In the Fall of 1904, Middlebury College had three fraternities oncampus: Chi Psi, Delta Kappa Epsilon, and Delta Upsilon. There also existed the Commons Club, a large neutral group of students.</p>
+					
+					<p>George E. Kimball, President of Commons Club met with Irving T. Coates and John Beecher [two fellow members] to discuss forming a fraternity. They met in Kimball's room, Room 14 of Old Painter Hall (pictured below), on the Middlebury College campus. After several meetings, they approached seven other members of the Commons Club to form the nucleus of a new fraternity: Thomas H. Bartley, Pierce W. Darrow, Benjamin E. Farr, Gideon R. Norton, Gino A. Ratti, Chester M. Walch and Roy D. Wood.</p>	
+					
+					<p>On May 17, 1905, these ten met again in room 14 of Old Painter Hall as the Charter Members of the newly formed Kappa Delta Rho Fraternity. They left school and did not publicly announce the formation of the new Fraternity until the fall.</p>
+
+					<p>Kimball, Walch &amp; Ratti met and drafted a ritual, open motto, and a constitution </p>
+
+					<p>Walch created secret motto and password. They chose Roman nomenclature for fraternity positions, to emulate stern Roman values. </p>
+
+					<p>Ratti designed coat of arms [had previous art experience] and helped develop the ritual, chose colors and flower.</p>
+
+					<p>During the first year, a representative from Delta Tau Delta met with KDR to discuss absorbing KDR into DTD. The Founders had expressed some interest in joining a national fraternity, but some decided against this idea and in Kimball's words: "We had decided that we preferred 'to paddle our own canoe and took no further action in the matter."</p>
+
+					<p>Beecher and Coates did not return to Middlebury for the next year. The Fraternity found support from Henry Harvey, a faculty member at Middlebury, who helped and assisted the chapter and became their first Honorary member.</p>
+
+					<p>George E. Shaw '10 became the first new member.</p>
+
+					<p>The new fraternity did not have pledge classes or pledges but new members were part of delegations.</p>
+
+					<p>In 1909, the first issue of The Scroll (to become the Quill and Scroll) with 30 copies for undergraduates, alumni and honorary members was published.</p>
+
+					<p>By 1913, the fraternity was attracting larger delegations of 10-15 men.</p>
+
+					<p>They used furnished, off-campus meeting rooms for rush, social events and secret conclaves.</p>
+
 				</li> <!-- cd-service -->
 
 				<li class="cd-service cd-service-2">
-					<h2>Responsive Approach</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis pariatur tenetur quod veritatis nulla aspernatur architecto! Fugit, reprehenderit amet deserunt molestiae ut libero facere quasi velit perferendis ullam quis necessitatibus!</p>
+					<h2>Beyond Middlebury, 1912-1919</h2>
+					<p>Beta Chapter at Cornell University was founded on May 24, 1913, largely through the efforts of Harold A. Severy, Alpha '09, then a graduate student at Cornell.</p>
+
+					<p>The Beta men were a strong and enthusiastic group and assisted in the formation of Gamma Chapter at New York State College for Teachers (now SUNY Albany) on June 10, 1915 and with Delta Chapter at Colgate University.</p>
+
+					<p>John F. Hough, Alpha '14 was then a graduate student at Colgate and helped found Delta on February 22, 1917. Delta's faculty advisor, Dr. Frank C. Ewart, was made an Honorary Brother and later became National President. He helped Delta Chapter financially, purchased a house for the fraternity, and helped develop the National Fraternity. </p>
+
 				</li> <!-- cd-service -->
 
 				<li class="cd-service cd-service-3">
-					<h2>E-commerce</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis pariatur tenetur quod veritatis nulla aspernatur architecto! Fugit, reprehenderit amet deserunt molestiae ut libero facere quasi velit perferendis ullam quis necessitatibus!</p>
+					<h2>World War I, 1917-1919</h2>
+					<p>During World War I, chapter houses closed to become boarding houses for Army units.</p>
+
+					<p>Publication of The Scroll was suspended.</p>
+
+					<p>Alumni helped to sustain fraternity through the end of the war. After the war, chapter houses reopened and growth continued.</p>
+
+					<p>Dr. Ewart helped establish Epsilon Chapter at Franklin College, Indiana, on May 24, 1919. </p>
+
 				</li> <!-- cd-service -->
 
 				<li class="cd-service cd-service-4">
-					<h2>CMS Integration</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis pariatur tenetur quod veritatis nulla aspernatur architecto! Fugit, reprehenderit amet deserunt molestiae ut libero facere quasi velit perferendis ullam quis necessitatibus!</p>
+					<h2>Expansion in the 1920's</h2>
+					<p>Dr. Ewart was a dynamic force, almost a crusader. With assistance from "Scotty" Ottman from Alpha, Leo T. Wolford from Epsilon and others, fourteen (14) new chapters emerged in rapid succession. These included, Penn State University, University of Illinois, Purdue University, Bucknell University, Ohio State University, The University of California at Berkeley, University of Michigan, Indiana University, Colby College, Butler University, Gettysburg College, Lafayette College, Oregon State University, and Carnegie Institute of Technology [now Carnegie-Mellon University].</p>
+					<p>In 1921, KDR became a Junior Member of the North-American Interfraternity Conference (NIC) and in 1929 a Senior Member. </p>
+				</li> <!-- cd-service -->
+
+				<li class="cd-service cd-service-4">
+					<h2>The Depression</h2>
+					<p>By 1930 there were nineteen (19) chapters in ten (10) states and the Fraternities' first full time paid National Executive Secretary [later National Executive Director], Reginald M. Savage, Alpha '24, was hired </p>
+
+					<p>KDR was not immune to the economic effects of the depression. All but two chapters were sustained (Butler University and Carnegie Tech), due in large part to Alumni support.</p>
+
+					<p>At the 1932 Convention, the constitution and general structure changed to begin the process of moving towards incorporation.</p>
+
+					<p>In 1939, one new chapter, Upsilon Chapter was chartered at Fresno State College. </p>
+
+				</li> <!-- cd-service -->
+
+				<li class="cd-service cd-service-4">
+					<h2>World War II</h2>
+					<p>The Quill and Scroll continued publication and eventually included stories from the front and addresses of brothers overseas, resulting in reunions of brothers and chapters on the battlefield.</p>
+
+					<p>Seventy brothers lost their lives during World War II.</p>
+
+				</li> <!-- cd-service -->
+
+				<li class="cd-service cd-service-4">
+					<h2>After 1946, Reactivation, Expansion, and Innovation</h2>
+					<p>The 1946 Convention focused on restoration of KDR's chapters and fifteen (15) reopened that fall.</p>
+
+					<p>Under guidance of Grand Consul John L. Blakeley, Eta '22, National Executive Secretary Fred B. Ensinger, Alpha \'45, and Expansion Chairman Frederic T. Closs, Rho \'51, several new chapters came into the fraternity.</p>
+
+					<p>On February 10, 1954, with the assistance of Orrin G. Judd, Delta '26, the Fraternity received a certificate of incorporation under New York State Membership Corporation Law, officially becoming The National Fraternity of Kappa Delta Rho, Incorporated.</p>
+
+					<p>In 1956, KDR celebrated her Semi-Centennial Celebration. The 45th Annual Convention was held in Breadloaf, Vermont and on September 1, 1956, a memorial plaque was unveiled and dedicated at Old Painter Hall on Middlebury's campus.</p>
+
+					<p>The History of Kappa Delta Rho 1905-1955 was completed and presented by KDR Historian George E. Shaw, Alpha '10.</p>
+
+					<p>Several new chapters were added during the late 1950's and 1960's.</p>
+
+					<p>On November 12, 1960, the KDR Trust [later KDR Foundation] formed, to promote educational, charitable, and literary purposes for the National Fraternity, her chapters, and all KDR members.</p>
+
+				</li> <!-- cd-service -->
+
+				<li class="cd-service cd-service-4">
+					<h2>On To The Centennial</h2>
+					<p>James F. Edgeworth, Sr., Psi \'56 becomes National President/Grand Worthy Consul in 1988, serving until 1998. 
+					The National Executive Secretarytitle is changed to National Executive Director, who also serves as Grand Senior Tribune.</p>
+
+					<p>Risk Management and poor image issues resulted in problems for all fraternities and the closing of Greek Systems at several college and universities due to risk and image issues. In 1983, Xi chapter is closed when Colby College eliminates their entire Greek system.</p>
+
+					<p>All-Male institutions come under close scrutiny, resulting in administrations at several colleges deciding that all campus organizations would be coeducational. Middlebury College enforces this policy in 1989, causing Alpha Chapter to make a choice.</p>
+
+					<p>The Brothers of Alpha decided it was a better decision to accept women into their brotherhood that to cease to exist.</p>
+
+					<p>
+					Alpha Chapter pledged and initiated their first female members in 1989 and this resulted in the loss of affiliation for Alpha Chapter with the National Fraternity, until a solution could be implemented.</p>
+
+					<p>In 1997, after years of proposals and debating, a proposal to create the Kappa Delta Rho Society allowing KDR to exist at Middlebury and allowing the National fraternity to maintain her identity as a male fraternal organization.</p>
+
+					<p>This was only allowed at Alpha Chapter. The KDR Society at Middlebury received its charter in October 2000 from Grand Worthy Consul Robert L. Swinney, Sigma \'58.</p>
+					
+					<p>KDR is the only Greek organization at Middlebury with a national affiliation.</p>
+
+				</li> <!-- cd-service -->
+
+				<li class="cd-service cd-service-4">
+					<h2>The Centennial and Beyond</h2>
+					<p>Since 1905, KDR has initiated over 23,000 Members with some 17,000 Living Alumni.</p>
+
+					<p>The Centennial year of 2005 was celebrated at the 2005 Convention in Baltimore, Maryland, where the Ten Founders were honored posthumously with induction into the Ordo Honoris by Grand Consul Joshua L. Smith, Alpha Beta \'93.</p>
+
+					<p>KDR continues to develop new advancements in chapter operations, educational programming, and long term member development including implementation of the KDR Wilderness Institute and The Path membership development programs in 2007. </p>
+
+					<p>The current National President/Grand Worthy Consul is Shawn Hoke, Phi Alpha, \'95. </p>
+
+					<p>Michael Mueller, Eta \'95, serves as the current President of the Kappa Delta Rho Foundation. </p>
+
+					<p>In 2007, Joseph Rosenberg, Xi Alpha \'96, was hired as the new Executive Director of Kappa Delta Rho. </p>
+
 				</li> <!-- cd-service -->
 
 				<li class="cd-service cd-service-divider"></li>
@@ -70,8 +184,12 @@ Template Name: History page
 			bodyBackground(topValueFillingBlocks);
 		});
 
-		$("#filler").css("height",$("#cd-container").height())
+		$("#filler").css("height",$("#cd-container").height());
 	});
+
+	$(window).resize(function(){
+		$("#filler").css("height",$("#cd-container").height());
+	})
 
 	function updateOnFocusItem(items) {
 		items.each(function(){
