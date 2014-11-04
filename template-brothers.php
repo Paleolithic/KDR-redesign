19997b3d3dd939485e54f74058a27107245d8797
Paleolithic <tjb2597@rit.edu>
2014-11-04 00:55:44 -0500

    Minor fix in eboard image pulling

--- template-brothers.php
+++ template-brothers.php
@@ -15,35 +15,35 @@ Template Name: Brothers page
 			</header>
 			<h2 class="sixteen columns">Executive Board</h2>
 			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/test/" . $praetor_info->pin . ".jpg"; ?>
+				<?php $url = get_template_directory_uri() . "/images/brothers/" . $praetor_info->pin . ".jpg"; ?>
 				<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
 				<h4>Praetor</h4>
 				<div class="border-bottom transition-right"></div>
 				<p><span><?php echo $praetor_info->first_name; ?></span><span><?php echo $praetor_info->last_name; ?></span><span>#<?php echo $praetor_info->pin; ?></span></p>		
 			</div>
 			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/test/" . $jt_info->pin . ".jpg"; ?>
+				<?php $url = get_template_directory_uri() . "/images/brothers/" . $jt_info->pin . ".jpg"; ?>
 				<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
 				<h4>Junior Tribune</h4>
 				<div class="border-bottom transition-right"></div>
 				<p><span><?php echo $jt_info->first_name; ?></span><span><?php echo $jt_info->last_name; ?></span><span>#<?php echo $jt_info->pin; ?></span></p>		
 			</div>
 			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/test/" . $consul_info->pin . ".jpg"; ?>
+				<?php $url = get_template_directory_uri() . "/images/brothers/" . $consul_info->pin . ".jpg"; ?>
 				<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
 				<h4>Consul</h4>
 				<div class="border-bottom transition-center"></div>
 				<p><span><?php echo $consul_info->first_name; ?></span><span><?php echo $consul_info->last_name; ?></span><span>#<?php echo $consul_info->pin; ?></span></p>		
 			</div>
 			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/test/" . $st_info->pin . ".jpg"; ?>
+				<?php $url = get_template_directory_uri() . "/images/brothers/" . $st_info->pin . ".jpg"; ?>
 				<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
 				<h4>Senior Tribune</h4>
 				<div class="border-bottom transition-left"></div>
 				<p><span><?php echo $st_info->first_name; ?></span><span><?php echo $st_info->last_name; ?></span><span>#<?php echo $st_info->pin; ?></span></p>	
 			</div>
 			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/test/" . $quaestor_info->pin . ".jpg"; ?>
+				<?php $url = get_template_directory_uri() . "/images/brothers/" . $quaestor_info->pin . ".jpg"; ?>
 				<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
 				<h4>Quaestor</h4>
 				<div class="border-bottom transition-left"></div>
@@ -73,8 +73,12 @@ Template Name: Brothers page
 					if($user_info->first_name && $user_info->last_name && $user_info->active_member == "active"){ ?>
 						<div class="one-fifth column active-fifths">
 							<!-- $directory = bloginfo('stylesheet_directory') . '/images/brothers/test/' . $user_info->pin . '.jpg' -->
-							<?php $url = get_template_directory_uri() . "/images/brothers/" . $user_info->pin . ".jpg"; ?>
-							<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
+							<?php $url = get_template_directory_uri() . "/images/brothers/" . $user_info->pin . ".jpg"; 
+								  $missing_url = get_template_directory_uri() . "/images/brothers/no_pic.jpg"; 
+								  $check_path = TEMPLATEPATH . "/images/brothers/" . $user_info->pin . ".jpg";
+							?>
+							<?php if(file_exists ( $check_path)){ ?><div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
+							<?php } else{ ?><div class="portrait" style="background-image: url('<?php echo $missing_url; ?>');"></div><?php } ?>
 							<p><span><?php echo $user_info->first_name ?></span><span><?php echo $user_info->last_name ?></span><span>#<?php echo $user_info->pin ?></span></p> 
 						</div>				
 			<?php
@@ -93,8 +97,12 @@ Template Name: Brothers page
 					if($user_info->first_name && $user_info->last_name && $user_info->active_member != "active"){ ?>
 						<div class="one-fifth column active-fifths">
 							<!-- $directory = bloginfo('stylesheet_directory') . '/images/brothers/test/' . $user_info->pin . '.jpg' -->
-							<?php $url = get_template_directory_uri() . "/images/brothers/" . $user_info->pin . ".jpg"; ?>
-							<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
+							<?php $url = get_template_directory_uri() . "/images/brothers/" . $user_info->pin . ".jpg"; 
+								  $missing_url = get_template_directory_uri() . "/images/brothers/no_pic.jpg"; 
+								  $check_path = TEMPLATEPATH . "/images/brothers/" . $user_info->pin . ".jpg";
+							?>
+							<?php if(file_exists ( $check_path)){ ?><div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
+							<?php } else{ ?><div class="portrait" style="background-image: url('<?php echo $missing_url; ?>');"></div><?php } ?>
 							<p><span><?php echo $user_info->first_name ?></span><span><?php echo $user_info->last_name ?></span><span>#<?php echo $user_info->pin ?></span></p> 
 						</div>				
 			<?php
