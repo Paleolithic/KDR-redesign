8a2a05d02ed767c33372311c4f401abd3b852a10
Paleolithic <tjb2597@rit.edu>
2015-02-18 01:42:07 -0500

    picture sizes

--- template-brothers.php
+++ template-brothers.php
@@ -4,7 +4,7 @@ Template Name: Brothers page
 */
 ?>
 <?php get_header(); ?>
-<?php include (TEMPLATEPATH . '/includes/position-variables.php'); ?>
+
 <main class="brothers">
 	<section class="e-board">
 		<div class="container">
@@ -14,83 +14,7 @@ Template Name: Brothers page
 				</p>
 			</header>
 			<h2 class="sixteen columns">Executive Board</h2>
-			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/" . $praetor_info->pin . ".jpg";  
-					  $missing_url = get_template_directory_uri() . "/images/brothers/no_pic.jpg"; 
-					  $check_path = TEMPLATEPATH . "/images/brothers/" . $praetor_info->pin . ".jpg"; ?>
-				<?php if(file_exists ( $check_path)){ ?>
-					<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
-				<?php } else{ ?>
-					<div class="portrait" style="background-image: url('<?php echo $missing_url; ?>');"></div>
-				<?php } ?>
-				<h4>Praetor</h4>
-				<div class="border-bottom transition-right"></div>
-				<p>
-					<span><?php echo $praetor_info->first_name; ?></span>
-					<span><?php echo $praetor_info->last_name; ?></span>
-					<span>#<?php echo $praetor_info->pin; ?></span>
-				</p>		
-			</div>
-			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/" . $jt_info->pin . ".jpg";
-					  $check_path = TEMPLATEPATH . "/images/brothers/" . $jt_info->pin . ".jpg"; ?>
-				<?php if(file_exists ( $check_path)){ ?>
-					<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
-				<?php } else{ ?>
-					<div class="portrait" style="background-image: url('<?php echo $missing_url; ?>');"></div>
-				<?php } ?>
-				<h4>Junior Tribune</h4>
-				<div class="border-bottom transition-right"></div>
-				<p>
-					<span><?php echo $jt_info->first_name; ?></span>
-					<span><?php echo $jt_info->last_name; ?></span>
-					<span>#<?php echo $jt_info->pin; ?></span>
-				</p>		
-			</div>
-			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/" . $consul_info->pin . ".jpg";  
-					  $check_path = TEMPLATEPATH . "/images/brothers/" . $consul_info->pin . ".jpg"; ?>
-  				<?php if(file_exists ( $check_path)){ ?>
-  					<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
-				<?php } else{ ?>
-					<div class="portrait" style="background-image: url('<?php echo $missing_url; ?>');"></div>
-				<?php } ?>
-				<h4>Consul</h4>
-				<div class="border-bottom transition-center"></div>
-				<p>
-					<span><?php echo $consul_info->first_name; ?></span>
-					<span><?php echo $consul_info->last_name; ?></span>
-					<span>#<?php echo $consul_info->pin; ?></span>
-				</p>		
-			</div>
-			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/" . $st_info->pin . ".jpg";  
-					  $check_path = TEMPLATEPATH . "/images/brothers/" . $st_info->pin . ".jpg"; ?>				
-				<?php if(file_exists ( $check_path)){ ?>	
-					<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
-				<?php } else{ ?>
-					<div class="portrait" style="background-image: url('<?php echo $missing_url; ?>');"></div>
-				<?php } ?>
-				<h4>Senior Tribune</h4>
-				<div class="border-bottom transition-left"></div>
-				<p>
-					<span><?php echo $st_info->first_name; ?></span>
-					<span><?php echo $st_info->last_name; ?></span>
-					<span>#<?php echo $st_info->pin; ?></span>
-				</p>	
-			</div>
-			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/" . $quaestor_info->pin . ".jpg";  
-					  $check_path = TEMPLATEPATH . "/images/brothers/" . $quaestor_info->pin . ".jpg"; ?>			
-				<?php if(file_exists ( $check_path)){ ?>
-					<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
-				<?php } else{ ?>
-					<div class="portrait" style="background-image: url('<?php echo $missing_url; ?>');"></div>
-				<?php } ?>
-				<h4>Quaestor</h4>
-				<div class="border-bottom transition-left"></div>
-				<p><span><?php echo $quaestor_info->first_name; ?></span><span><?php echo $quaestor_info->last_name; ?></span><span>#<?php echo $quaestor_info->pin; ?></span></p>	
-			</div>
+			<?php include (TEMPLATEPATH . '/includes/eboard.php'); ?>
 		</div>
 	</section>
 	<?php $approvedusers = get_users(array(
--- template-home.php
+++ template-home.php
@@ -132,56 +132,7 @@ Template Name: Home page
 				<p>These are the gentlemen who keep our fraternity alive and well. They each have a specific job in managing our day to day activities, and do their best to keep us efficient
 				</p>
 			</header>
-			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/" . $praetor_info->pin . ".jpg";  
-					  $missing_url = get_template_directory_uri() . "/images/brothers/no_pic.jpg"; 
-					  $check_path = TEMPLATEPATH . "/images/brothers/" . $praetor_info->pin . ".jpg";
-				?>
-				<?php if(file_exists ( $check_path)){ ?><div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
-				<?php } else{ ?><div class="portrait" style="background-image: url('<?php echo $missing_url; ?>');"></div><?php } ?>
-				<h4>Praetor</h4>
-				<div class="border-bottom transition-right"></div>
-				<p><span><?php echo $praetor_info->first_name; ?></span><span><?php echo $praetor_info->last_name; ?></span><span>#<?php echo $praetor_info->pin; ?></span></p>		
-			</div>
-			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/" . $jt_info->pin . ".jpg";  
-					  $missing_url = get_template_directory_uri() . "/images/brothers/no_pic.jpg"; 
-					  $check_path = TEMPLATEPATH . "/images/brothers/" . $jt_info->pin . ".jpg";
-				?>
-				<?php if(file_exists ( $check_path)){ ?><div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
-				<?php } else{ ?><div class="portrait" style="background-image: url('<?php echo $missing_url; ?>');"></div><?php } ?>
-				<h4>Junior Tribune</h4>
-				<div class="border-bottom transition-right"></div>
-				<p><span><?php echo $jt_info->first_name; ?></span><span><?php echo $jt_info->last_name; ?></span><span>#<?php echo $jt_info->pin; ?></span></p>		
-			</div>
-			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/" . $consul_info->pin . ".jpg";  
-					  $missing_url = get_template_directory_uri() . "/images/brothers/no_pic.jpg"; 
-					  $check_path = TEMPLATEPATH . "/images/brothers/" . $consul_info->pin . ".jpg";
-				?>				<?php if(file_exists ( $check_path)){ ?><div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
-				<?php } else{ ?><div class="portrait" style="background-image: url('<?php echo $missing_url; ?>');"></div><?php } ?>
-				<h4>Consul</h4>
-				<div class="border-bottom transition-center"></div>
-				<p><span><?php echo $consul_info->first_name; ?></span><span><?php echo $consul_info->last_name; ?></span><span>#<?php echo $consul_info->pin; ?></span></p>		
-			</div>
-			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/" . $st_info->pin . ".jpg";  
-					  $missing_url = get_template_directory_uri() . "/images/brothers/no_pic.jpg"; 
-					  $check_path = TEMPLATEPATH . "/images/brothers/" . $st_info->pin . ".jpg";
-				?>				<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
-				<h4>Senior Tribune</h4>
-				<div class="border-bottom transition-left"></div>
-				<p><span><?php echo $st_info->first_name; ?></span><span><?php echo $st_info->last_name; ?></span><span>#<?php echo $st_info->pin; ?></span></p>	
-			</div>
-			<div class="one-fifth column e-board-fifths">
-				<?php $url = get_template_directory_uri() . "/images/brothers/" . $quaestor_info->pin . ".jpg";  
-					  $missing_url = get_template_directory_uri() . "/images/brothers/no_pic.jpg"; 
-					  $check_path = TEMPLATEPATH . "/images/brothers/" . $quaestor_info->pin . ".jpg";
-				?>				<div class="portrait" style="background-image: url('<?php echo $url; ?>');"></div>
-				<h4>Quaestor</h4>
-				<div class="border-bottom transition-left"></div>
-				<p><span><?php echo $quaestor_info->first_name; ?></span><span><?php echo $quaestor_info->last_name; ?></span><span>#<?php echo $quaestor_info->pin; ?></span></p>	
-			</div>
+			<?php include (TEMPLATEPATH . '/includes/eboard.php'); ?>
 		</div>
 	</section>
 	<section class="blogs">
